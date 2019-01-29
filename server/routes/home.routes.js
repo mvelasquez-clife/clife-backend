4e2250a78fee0d2aa434fc71e2854ad003e7aa9b
Jhon <jtipismana.rodicar@gmail.com>
2019-01-29 10:19:59 -0500

    varias modificaciones para inicio.html

--- server/routes/home.routes.js
+++ server/routes/home.routes.js
@@ -5,4 +5,6 @@ const homeController = require('../controllers/home.controller');
 
 router.get('/menu/:alias/:empresa',homeController.getMenu);
 
+router.post('/menusearch',homeController.getmenu_search);
+
 module.exports = router;
