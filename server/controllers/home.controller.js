ab89720d68129789685c8d90553e83d702e4765c
Jhon <jtipismana.rodicar@gmail.com>
2019-01-29 09:32:52 -0500

    se agrego una funcion getmenu_search()=> devuelve json de los modulos filtrados en el input de busqueda.

--- server/controllers/home.controller.js
+++ server/controllers/home.controller.js
@@ -4,10 +4,10 @@ const o2x = require('object-to-xml');
 
 const homeController = {
     getMenu: (req, res) => {
-        const { id } = req.query;
-        const { alias, empresa } = req.params;
+        const {id} = req.query;
+        const {alias, empresa} = req.params;
         oracledb.getConnection(dbParams, (err, conn) => {
-            if(err) {
+            if (err) {
                 console.error(err);
                 return;
             }
@@ -15,43 +15,105 @@ const homeController = {
                 outFormat: oracledb.OBJECT
             };
             const query1 = 'call pack_venta.sm_activar_empresa(:alias)';
-            const params1 = { alias: { val: alias } };
+            const params1 = {alias: {val: alias}};
             conn.execute(query1, params1, responseParams, (error, result) => {
-                if(error) {
+                if (error) {
                     console.error(error);
                     conn.close();
                     return;
                 }
             });
             const query2 = "select co_menu_sistema as \"id\",de_nombre as \"text\", case st_tipo when 'm' then 1 else 0 end as \"child\" from v_generar_menu where de_alias = :alias and co_empresa = :empresa and co_antecesor = :codigo order by st_tipo desc";
-            const params2 = { alias: { val: alias }, empresa: { val: empresa }, codigo: { val: id } };
+            const params2 = {alias: {val: alias}, empresa: {val: empresa}, codigo: {val: id}};
             conn.execute(query2, params2, responseParams, (error, result) => {
-                if(error) {
+                if (error) {
                     console.error(error);
                     conn.close();
                     return;
                 }
                 /*return res.json({
-                    id: id,
-                    items: result.rows
-                });*/
+                 id: id,
+                 items: result.rows
+                 });*/
                 var arr = [];
-                for(var i in result.rows) {
+                for (var i in result.rows) {
                     arr.push({
                         '@': result.rows[i]
                     });
                 }
                 res.set('Content-Type', 'text/xml');
                 res.send(o2x({
-                    '?xml version="1.0" encoding="utf-8"?' : null,
+                    '?xml version="1.0" encoding="utf-8"?': null,
                     tree: {
-                        '@': { id: id == 'MAIN' ? 0 : id },
+                        '@': {id: id == 'MAIN' ? 0 : id},
                         item: arr
                     }
                 }));
             });
         });
+    },
+
+    getmenu_search: (req, res) => {
+        const {id} = req.query;
+        const {alias, empresa, txtsearch} = req.body;
+        oracledb.getConnection(dbParams, (err, conn) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const responseParams = {
+                outFormat: oracledb.OBJECT
+            };
+            const query1 = 'call pack_venta.sm_activar_empresa(:alias)';
+            const params1 = {alias: {val: alias}};
+            conn.execute(query1, params1, responseParams, (error, result) => {
+                if (error) {
+                    console.error(error);
+                    conn.close();
+                    return;
+                }
+            });
+
+            const query2 = "select gen.co_menu_sistema as \"id\",'>>' as \"flecha\" , '<div class=\"classfont\" >'||gen.de_nombre||'</div>' as \"name\", '<div class=\"classfont\" >'||sist.de_nombre||'</div>' as \"padre\" from v_generar_menu gen left join  sg_menu_sist_m sist on gen.co_antecesor = sist.co_menu_sistema   where gen.de_alias=:alias and gen.co_empresa =:empresa and gen.st_tipo='i' and gen.es_vigencia='Activo' and upper(gen.de_nombre) like upper(:text) order by gen.st_tipo desc ";
+            const params2 = {alias: {val: alias}, empresa: {val: empresa}, text: {val: '%' + txtsearch + '%'}};
+            conn.execute(query2, params2, responseParams, (error, result) => {
+                if (error) {
+                    console.error(error);
+                    conn.close();
+                    return;
+                }
+                var l_items = [];
+
+                for (var i in result.rows) {
+                    l_items.push(result.rows[i]);
+                }
+                //comprobar si obtuve resultado
+                if (l_items.length > 0) {
+                    res.json({
+                        state: 'success',
+                        data: {l_items
+                        }
+                    });
+                } else {
+                    res.json({
+                        state: 'false',
+                        data: {
+                            l_items :                            [{'id': 0, 'name': 'No se encontraron concidencias...'}]
+                        }
+                    });
+                }
+
+                //  res.json({result.rows});
+//                 res.
+//                res.set('Content-Type', 'application/json');
+//                res.send(o2x({arr}));
+
+
+            });
+        });
     }
+
+
 };
 
 module.exports = homeController;
