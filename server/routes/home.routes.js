17711b75aad0ef7cb1b24e02df99848aa1bbfac1
Jhon <jtipismana.rodicar@gmail.com>
2019-01-29 09:38:38 -0500

    Revert "se agrego una funcion getmenu_search()=> devuelve json  de los modulos filtrados en el input de busqueda."
    This reverts commit ab89720d68129789685c8d90553e83d702e4765c.

--- server/routes/home.routes.js
+++ server/routes/home.routes.js
@@ -5,6 +5,4 @@ const homeController = require('../controllers/home.controller');
 
 router.get('/menu/:alias/:empresa',homeController.getMenu);
 
-router.post('/menusearch',homeController.getmenu_search);
-
 module.exports = router;
