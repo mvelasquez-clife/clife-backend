a7722baa6104001461c8f164768b5b00e8f99800
Jhon <jtipismana.rodicar@gmail.com>
2019-02-01 09:51:01 -0500

    varias modificaciones profile - cuenta

--- client/controllers/intranet.controller.js
+++ client/controllers/intranet.controller.js
@@ -1,41 +1,5 @@
 var path = require('path');
 //const ejs = require('ejs');
-
-const intranetController = {
-    home: (req, res) => {
-        res.sendFile(path.resolve('client/views/login/login.html'));
-    },
-    inicio: (req, res) => {
-        res.sendFile(path.resolve('client/views/home/inicio.html'));
-    },
-
-    CargarModulo: (req, res) => {
-        const modulo = req.params.id;
-        var subcarpeta = '';
-        const prefijo = modulo.substring(0, 2);
-        switch (prefijo) {
-            case 'MA':
-                subcarpeta = 'maestros';
-                break;
-            case 'AD':
-                subcarpeta = 'administracion';
-                break;
-            default:
-                '';
-        }
-        res.sendFile(path.resolve('client/views/modulos/' + subcarpeta + '/' + modulo + '.html'));
-        /*
-         ejs.render(path.resolve('client/views/modulos/' + subcarpeta + '/' + modulo + '.html'), data);*/
-        const data = {
-            path: '/modulos/' + subcarpeta + '/' + modulo + '/'
-        };
-        res.render(path.resolve('client/views/modulos/' + subcarpeta + '/' + modulo + '.ejs'), data);
-    }
-};
-
-var path = require('path');
-var fs = require('fs');
-
 const intranetController = {
     home: (req, res) => {
         res.sendFile(path.resolve('client/views/login/login.html'));
