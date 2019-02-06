4abff2eb3cb4210f2a8ea1bd9f42ccb0701a9de8
mvelasquez-clife <mvelasquez@corporacionlife.com.pe>
2019-02-05 19:21:20 -0500

    avance - finanzas/BA010305
    busqueda, lista de planillas, detalle de planillas, graficos, permisos

--- server/controllers/finanzas/BA010305.controller.js
+++ server/controllers/finanzas/BA010305.controller.js
@@ -8,8 +8,8 @@ const responseParams = {
 
 const ba010305Controller = {
     ComboPeriodos: (req, res) => {
+        const { empresa } = req.params;
         oracledb.getConnection(dbParams, (err, conn) => {
-            const { empresa } = req.params;
             if(err) {
                 console.error(err);
                 return;
@@ -26,6 +26,98 @@ const ba010305Controller = {
                 }
                 res.set('Content-Type', 'text/xml');
                 res.send(xmlParser.renderCombo(result.rows));
+            });
+        });
+    },
+    ListaPlanillas: (req, res) => {
+        const { empresa, cobrador, periodo } = req.params;
+        oracledb.getConnection(dbParams, (err, conn) => {
+            if(err) {
+                console.error(err);
+                return;
+            }
+            const query = "select co_planilla_cobranza,co_caja,to_char(fe_creacion_planilla,'yyyy-mm-dd') fe_creacion_planilla,to_char(fe_cierre,'yyyy-mm-dd') fe_cierre,co_moneda,de_moneda,co_tipo_docu_administr,de_tipo_doc_admin,im_tarjeta,im_total,im_valores,im_deposito_total,im_diferencia,st_conciliado,st_liquidado,st_recibo,co_periodo,to_char(fe_aprobacion_cierre,'yyyy-mm-dd') fe_aprobacion_cierre,es_vigencia from table(pack_new_finanzas_no_tocar.f_list_plan_cobranza_cobr_per(:empresa,:cobrador,:periodo))";
+            const params = {
+                empresa: { val: empresa },
+                cobrador: { val: cobrador },
+                periodo: { val: periodo }
+            };
+            conn.execute(query, params, responseParams, (error, result) => {
+                if(error) {
+                    console.error(error);
+                    conn.close();
+                    return;
+                }
+                res.set('Content-Type', 'text/xml');
+                res.send(xmlParser.renderXml(result.rows));
+            });
+        });
+    },
+    Grafico12: (req, res) => {
+        const { empresa, cobrador, periodo } = req.body;
+        oracledb.getConnection(dbParams, (err, conn) => {
+            if(err) {
+                res.json({
+                    state: 'error',
+                    message: err
+                });
+                return;
+            }
+            const query = "select co_tipo_doc_administr codigo,de_tipo_doc_administr tipo,num_primero importe,num_segundo deposito from table(pack_new_finanzas_no_tocar.f_grafico_planillas_1(:empresa,:cobrador,:periodo))";
+            const params = {
+                empresa: { val: empresa },
+                cobrador: { val: cobrador },
+                periodo: { val: periodo }
+            };
+            conn.execute(query, params, responseParams, (error, result) => {
+                if(error) {
+                    res.json({
+                        state: 'error',
+                        message: error
+                    });
+                    conn.close();
+                    return;
+                }
+                res.json({
+                    state: 'success',
+                    data: {
+                        grafico: result.rows
+                    }
+                });
+            });
+        });
+    },
+    Grafico3: (req, res) => {
+        const { empresa, cobrador, periodo } = req.body;
+        oracledb.getConnection(dbParams, (err, conn) => {
+            if(err) {
+                res.json({
+                    state: 'error',
+                    message: err
+                });
+                return;
+            }
+            const query = "select co_tipo_doc_administr codigo,de_tipo_doc_administr tipo,num_primero conciliadas,num_segundo planillas from table(pack_new_finanzas_no_tocar.f_grafico_planillas_2(:empresa,:cobrador,:periodo))";
+            const params = {
+                empresa: { val: empresa },
+                cobrador: { val: cobrador },
+                periodo: { val: periodo }
+            };
+            conn.execute(query, params, responseParams, (error, result) => {
+                if(error) {
+                    res.json({
+                        state: 'error',
+                        message: error
+                    });
+                    conn.close();
+                    return;
+                }
+                res.json({
+                    state: 'success',
+                    data: {
+                        grafico: result.rows
+                    }
+                });
             });
         });
     }
--- server/routes/finanzas/BA010305.routes.js
+++ server/routes/finanzas/BA010305.routes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const ba010305Controller = require('../../controllers/finanzas/BA010305.controller');
 
 router.get('/combo-periodos/:empresa', ba010305Controller.ComboPeriodos);
+router.get('/lista-planillas/:empresa/:cobrador/:periodo', ba010305Controller.ListaPlanillas);
+router.post('/grafico-12', ba010305Controller.Grafico12);
+router.post('/grafico-3', ba010305Controller.Grafico3);
 
 module.exports = router;
